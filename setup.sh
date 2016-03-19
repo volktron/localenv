1c10f1e2f7f7565072799e53fade5ec94d050e04
Henrik Volckmer <danorux@gmail.com>
2016-03-19 12:10:56 -0400

    Initial commit.

---
+++ setup.sh
@@ -0,0 +1,15 @@
+echo Installing nginx
+url="http://nginx.org/download/"
+curl -s nginx.org/en/download.html | grep --max-count=1 -Po 'nginx[^"]{1,}.zip' | head -n 1 | xargs echo "$url$1" | tr -d ' ' | xargs curl > nginx.zip
+
+unzip nginx.zip > /dev/null
+mkdir nginx
+mv nginx-*/* nginx
+rmdir nginx-*
+
+echo Installing PHP
+url="http://windows.php.net/downloads/releases/"
+curl -s http://windows.php.net/download | tac | tac | grep --max-count=1 -Po 'php[^"]{1,}-x64.zip' | xargs echo "$url$1" | tr -d ' ' | xargs curl > php.zip
+
+unzip php.zip -d php > /dev/null
+
